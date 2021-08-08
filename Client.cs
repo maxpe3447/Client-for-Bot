ea1bedfc86966743992a1ce1a538ee4eaeef91c7
Max <m.pech.pr@gmail.com>
2021-08-08 16:44:00 +0300

    working client

---
+++ Client.cs
@@ -0,0 +1,234 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace Tg_Bot
+{
+    class Client
+    {
+        public string Ip { get; set; } = "80.87.192.62";//"192.168.1.9";
+        public int Port { get; set; } = 30678;
+        private Socket clientSocket;
+        private IPEndPoint endPoint;
+        private string okCode;
+        public List<string> ServerStatusOperation = new List<string>();
+        public bool isConnect { get; set; } = false;
+        public Client()
+        {
+            Connect();
+
+        }
+        private void StartSocket() { clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); }
+        private void StopSocket()
+        {
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+        public string[] GetFilesName()
+        {
+            if (okCode == null) return null;
+            StartSocket();
+            clientSocket.Connect(endPoint);
+            ServerStatusOperation.Clear();
+            
+            SendData(((int)CodeForServer.GetFiles).ToString());
+            Logger_($"Operation {CodeForServer.GetFiles}", false);
+
+            string files = AnswerFromServer();
+            Logger_("Get files", false);
+
+            StopSocket();
+
+            return files.Split('|');
+        }
+
+        public string GetTextFromFile(string fileName)
+        {
+            if (okCode == null) return null;
+
+            StartSocket();
+            clientSocket.Connect(endPoint);
+            ServerStatusOperation.Clear();
+
+            SendData(((int)CodeForServer.GetTextFromFile).ToString());
+            Logger_($"Operation {CodeForServer.GetTextFromFile}");
+
+            SendData(fileName);
+
+            string text = AnswerFromServer();
+            Logger_("Geting text", false);
+
+            StopSocket();
+            return text;
+        }
+
+        public void SetTextFromFile(string fileName, string text)
+        {
+            if (okCode == null) return;
+
+            StartSocket();
+            clientSocket.Connect(endPoint);
+            ServerStatusOperation.Clear();
+
+            SendData(((int)CodeForServer.SetTextFromFile).ToString());
+            Logger_($"Operation {CodeForServer.SetTextFromFile}");
+
+
+            SendData(fileName);
+
+            Logger_("Send file");
+
+            SendData(text);
+
+            StopSocket();
+        }
+        public void CreateNewFile(string fileName)
+        {
+            if (okCode == null) return;
+
+            StartSocket();
+            clientSocket.Connect(endPoint);
+            ServerStatusOperation.Clear();
+
+            SendData(((int)CodeForServer.CreateNewFile).ToString());
+            Logger_($"Operation {CodeForServer.CreateNewFile}");
+
+
+            SendData(fileName);
+
+            Logger_("Create!");
+
+            StopSocket();
+
+        }
+
+        public void CreateEvent(string dateTime, string message)
+        {
+            if (okCode == null) return;
+
+            StartSocket();
+            clientSocket.Connect(endPoint);
+            ServerStatusOperation.Clear();
+
+            SendData(((int)CodeForServer.CreateEvent).ToString());
+            Logger_($"Operation {CodeForServer.CreateEvent}");
+
+            SendData(dateTime);
+
+            Logger_("Send DateTime");
+
+            SendData(message);
+
+            StopSocket();
+        }
+
+        public void EditFileName(string oldFileName, string newFileName)
+        {
+            if (okCode == null) return;
+
+            StartSocket();
+            clientSocket.Connect(endPoint);
+            ServerStatusOperation.Clear();
+
+            SendData(((int)CodeForServer.EditFileName).ToString());
+            Logger_($"Operation {CodeForServer.EditFileName}");
+
+            SendData(oldFileName);
+            Logger_($"Send old name: {oldFileName}");
+
+            SendData(newFileName);
+            Logger_($"Send new name: {newFileName}");
+        }
+        public void DeleteFile(string fileName)
+        {
+            if (okCode == null) return;
+
+            StartSocket();
+            clientSocket.Connect(endPoint);
+            ServerStatusOperation.Clear();
+
+            SendData(((int)CodeForServer.DeleteFile).ToString());
+            Logger_($"Operation {CodeForServer.EditFileName}");
+
+            SendData(fileName);
+            Logger_($"Delete {fileName}");
+        }
+        public void Connect(string ip = null, int port = -1)
+        {
+            if (!isConnect)
+            {
+                ServerStatusOperation.Clear();
+                if (ip != null && port != -1)
+                {
+                    Port = port;
+                    Ip = ip;
+                }
+
+                endPoint = new IPEndPoint(IPAddress.Parse(Ip), Port);
+
+                StartSocket();
+                try
+                {
+                    clientSocket.Connect(endPoint);
+                }
+                catch
+                {
+                    ServerStatusOperation.Add("ERROR Conected!");
+                    return;
+                }
+
+                if (clientSocket.Poll(2000, SelectMode.SelectError))
+                {
+                    ServerStatusOperation.Add("ERROR Conected!");
+                    return;
+                }
+                SendData(((int)CodeForServer.ForConnect).ToString());
+
+                okCode = AnswerFromServer();
+                isConnect = true;
+                Logger_("CONNECTED!", false);
+
+                StopSocket();
+            }
+        }
+        private string AnswerFromServer()
+        {
+            int size = clientSocket.ReceiveBufferSize;
+            var buffer = new byte[size];
+            StringBuilder sizeOfAnswer = new StringBuilder();
+            do
+            {
+                size = clientSocket.Receive(buffer);
+                sizeOfAnswer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+            } while (clientSocket.Available > 0);
+
+            return sizeOfAnswer.ToString();
+        }
+        private void Logger_(string msg = "", bool isWaitAnswer = true)
+        {
+            if (isWaitAnswer)
+            {
+                if (AnswerFromServer() == okCode)
+                    ServerStatusOperation.Add(msg + "  -> Ok!");
+                else
+                    ServerStatusOperation.Add(msg + "  -> Bad!");
+                return;
+            }
+            ServerStatusOperation.Add(msg + "  -> Ok!");
+
+
+        }
+        private void SendData(string data)
+        {
+            byte[] byteText = Encoding.UTF8.GetBytes(data);
+            clientSocket.SendBufferSize = byteText.Length;
+            clientSocket.Send(byteText);
+        }
+    }
+    enum CodeForServer
+    {
+        None, ForConnect, GetFiles, GetTextFromFile, SetTextFromFile, CreateNewFile, CreateEvent, EditFileName, DeleteFile
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,155 @@ namespace ClientForBot
     /// </summary>
     public partial class MainWindow : Window
     {
+        Tg_Bot.Client client;
+        FileNameContextMenueMode fileNameContextMenueMode;
         public MainWindow()
         {
             InitializeComponent();
+            client = new Tg_Bot.Client();
+            if (client.ServerStatusOperation.Count != 0)
+                ShowLogs();
+
+            tbIp.Text = client.Ip;
+            tbPort.Text = client.Port.ToString();
         }
+
+        private void bCreateEvent_Click(object sender, RoutedEventArgs e)
+        {
+            
+            client.CreateEvent(dpDate.ToString(),tbInfo.Text);
+            foreach (var log in client.ServerStatusOperation)
+            {
+                tbLogs.AppendText("\n" + log);
+            }
+        }
+
+        private void bGetFileNames_Click(object sender, RoutedEventArgs e)
+        {
+            lvFileNames.Items.Clear();
+
+            string[] files = client.GetFilesName();
+
+            foreach (var log in client.ServerStatusOperation)
+            {
+                tbLogs.AppendText("\n"+log);
+            }
+            if (files != null)
+                foreach (var file in files)
+                {
+                    lvFileNames.Items.Add(file.Remove(0, "ServiceFiles/".Length));
+                }
+        }
+
+        private string lastSelectFileName;
+        private void lvFileNames_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                lastSelectFileName = lvFileNames.SelectedItems[0].ToString();
+                if (tbInfo != null) 
+                tbInfo.Text = client.GetTextFromFile(lastSelectFileName);
+
+                ShowLogs();
+            }
+        }
+
+        private void bSendInfo_Click(object sender, RoutedEventArgs e)
+        {
+            client.SetTextFromFile(lastSelectFileName, tbInfo.Text);
+            ShowLogs();
+        }
+
+        private void ShowLogs()
+        {
+            foreach (var log in client.ServerStatusOperation)
+            {
+                tbLogs.AppendText('\n' + log);
+            }
+        }
+
+        private void tbLogs_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            tbLogs.Text = "Logs:";
+        }
+
+
+        private void lvFileNames_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Right)
+            {
+               
+            }
+        }
+
+        private void bCreateFileEvent_Click(object sender, RoutedEventArgs e)
+        {
+            tbForSetName.Visibility = Visibility.Visible;
+            fileNameContextMenueMode = FileNameContextMenueMode.Create;
+        }
+
+        private void bEditFile_Click(object sender, RoutedEventArgs e)
+        {
+            tbForSetName.Visibility = Visibility.Visible;
+            fileNameContextMenueMode = FileNameContextMenueMode.Edit;
+
+        }
+
+        private void bReconnect_Click(object sender, RoutedEventArgs e)
+        {
+            client.Connect(tbIp.Text, int.Parse(tbPort.Text));
+            ShowLogs();
+        }
+
+        private void tbInfo_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Escape)
+            {
+                lastSelectFileName = string.Empty;
+                tbInfo.Text = string.Empty;
+            }
+        }
+
+        private void tbForSetName_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (tbForSetName.Visibility == Visibility.Visible)
+            {
+                switch (e.Key)
+                {
+                    case Key.Enter:
+                        string newName = tbForSetName.Text;
+
+                        if (fileNameContextMenueMode == FileNameContextMenueMode.Create)
+                            client.CreateNewFile(newName);
+                        else if (fileNameContextMenueMode == FileNameContextMenueMode.Edit)
+                            client.EditFileName(lvFileNames.SelectedItems[0].ToString(), newName);
+                        bGetFileNames_Click(null, null);
+
+                        tbForSetName.Text = string.Empty;
+                        tbForSetName.Visibility = Visibility.Hidden;
+                        break;
+                    case Key.Escape:
+                        tbForSetName.Text = string.Empty;
+                        tbForSetName.Visibility = Visibility.Hidden;
+                        break;
+                }
+            }
+        }
+
+        private void bDeleteFile_Click(object sender, RoutedEventArgs e)
+        {
+            client.DeleteFile(lvFileNames.SelectedItem.ToString());
+            bGetFileNames_Click(null, null);
+        }
+
+        private void tbForSetName_MouseEnter(object sender, MouseEventArgs e)
+        {
+            tbForSetName.Text = string.Empty;
+        }
+    }
+
+    enum FileNameContextMenueMode : byte
+    {
+        Create, Edit
     }
 }
